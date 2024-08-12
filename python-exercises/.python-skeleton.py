042aa2336a80d5e0754941330f6f51a27aa9ede7
Hexagoritm <139809645+Hexagorithm@users.noreply.github.com>
2024-08-12 10:35:01 +0200

    Yes

--- python-exercises/.python-skeleton.py
+++ python-exercises/.python-skeleton.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import sys  # for sys.argv
 
-def <func>(<args>):
-    return None
-
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    ret = <func>(*args)
-    print(f"For arguments: {args} the return is {ret}")
+print("")
